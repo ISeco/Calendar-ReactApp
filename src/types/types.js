b1c98629829027a1f3c4090a5b0886eb858f8dc0
Ignacio Seco <ignacio.seco@inacapmail.cl>
2021-11-15 21:10:20 -0300

    Delpiegue de React App

---
+++ src/types/types.js
@@ -0,0 +1,24 @@
+export const types = {
+
+    uiOpenModal: '[ui] Open Modal',
+    uiCloseModal: '[ui] Close Modal',
+
+    eventSetActive: '[event] Set Active',
+    eventLogout: '[event] Set Active Logout',
+
+    eventStartAddNew: '[event] Start add new',
+    eventAddNew: '[event] Add New',
+    eventCleanActiveEvent: '[event] Clear Active Event',
+    eventUpdated: '[event] Event Updated',
+    eventDeleted: '[event] Event Deleted',
+    eventLoaded: '[event] Events loaded',
+
+
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+}
